5b612cdb4fb2b2626ca9dfce01991272c8f5718c
6tjessil <67714344+6tjessil@users.noreply.github.com>
2020-07-23 15:18:07 +1200

    HTML tables are added
    *The music library page now uses HTML tables to display data. Currently it is displaying dummy data.

--- your_library.php
+++ your_library.php
@@ -16,27 +16,33 @@
   <div id="queryheader">
     <h1>Cypher to Room</h1> 
   </div>
-  <div class="content">
-    <table>
-      <tr>
-        <th>Cypher</th>
-        <th>Room</th>
-      </tr>
-      <?php
-      require_once("connect.php");
-      $query = "SELECT t.cypher, r.room
-                    FROM teacher AS t
-                    JOIN teacher_to_room tr ON t.cypher = tr.cypher
-                    JOIN room r ON r.room_id = tr.room_id ";
-      $rs = mysqli_query($con, $query);
-      if ($rs) {
-        while ($row = mysqli_fetch_array($rs)) {
-          echo "<tr><td>" . $row['cypher'] . "</td><td>" . $row['room'] . "</td></tr>";
-        }
-      }
-      ?>
-    </table>
-  </div>
+	<div class = "music_data_div">
+		<div class="data">
+			<table>
+			<tr>
+				<th>Cypher</th>
+				<th>Room</th>
+				<th>Room</th>
+				<th>Room</th>
+				<th>Room</th>
+				<th>Room</th>
+			</tr>
+			<?php
+			require_once("connect.php");
+			$query = "SELECT *
+						FROM teacher";
+			$rs = mysqli_query($con, $query);
+			if ($rs) {
+				while ($row = mysqli_fetch_array($rs)) {
+				echo "<tr><td>" . $row['cypher'] . "</td><td>" . $row['surname'] . "</td>
+				<td>" . $row['christian'] . "</td><td>" . $row['form'] . "</td><td>" . $row['mentor'] . "</td>
+				<td>" . $row['role_id'] . "</td></tr>";
+				}
+			}
+			?>
+			</table>
+		</div>
+	</div>
 </body>
 
 </html>
