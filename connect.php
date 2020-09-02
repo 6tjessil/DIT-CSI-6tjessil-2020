50a6cbdd72410679d4596d92fa63366401318f5a
6tjessil <67714344+6tjessil@users.noreply.github.com>
2020-09-02 13:23:51 +1200

    Cards centring fixed

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php //Used to connect to the database
-	$con = mysqli_connect('fdb26.awardspace.net','3083550_6tjessil','dojustly01','3083550_6tjessil');
+	//$con = mysqli_connect('localhost','root','dojustly01','3083550_6tjessil');
+	$con = mysqli_connect('localhost','root','dojustly01','3083550_6tjessil');
         if (mysqli_connect_errno())
         {
         echo "Failed to connect". mysqli_connect_error();
